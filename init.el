e235c974d0fc209af312a608406d542fd1c1d778
Evgeni Pavlidis <pavlidis@in.tum.de>
2014-06-11 11:15:29 +0200

    modified init.el to auto install missing packages

--- init.el
+++ init.el
@@ -62,12 +62,16 @@
 	("melpa" . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
 
-(unless (package-installed-p 'org)
-    (package-install 'org))
+(unless (package-installed-p 'save-packages)
+    (package-install 'save-packages))
+(install-saved-packages)
+
+;;; load initializatoin.org file
 (setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
 (mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
 
 
+;;; emacs auto settings
 (put 'downcase-region 'disabled nil)
 (put 'scroll-left 'disabled nil)
 (put 'dired-find-alternate-file 'disabled nil)
